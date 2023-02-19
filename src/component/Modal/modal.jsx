07e70574a4fcd5cc8a670b85dad2b05eddd6a1ac
SamedovRasul <samedovrasul7@gmail.com>
2023-02-19 16:11:08 +0400

    modal inputs

--- src/component/Modal/modal.jsx
+++ src/component/Modal/modal.jsx
@@ -3,6 +3,8 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+
 
 const style = {
   position: 'absolute',
@@ -28,10 +30,11 @@ const style = {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style} className='Modal-Box' >
-          <p>Join Outschool For Free</p>
-          <p>Explore any interest with 140,000+ classes, pay as low as $10 USD per class.</p>
-          <button> Sign Up with Email </button>
-          <p> Already have an account? <a href="*"> Log in </a> </p>
+          <Typography>Create Account</Typography>
+          <TextField required id="outlined-basic" label="Adult Full Name" variant="outlined" />
+          <TextField required id="outlined-basic" label="Adult Email" variant="outlined" />
+          <TextField required id="outlined-basic" label="Password" variant="outlined" />
+          <button> Join Metatesk </button>
         </Box>
       </Modal>
     </div>
